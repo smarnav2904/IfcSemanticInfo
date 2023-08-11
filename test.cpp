bbd18ff34736f3f68510dc0354a5045d2c33db73
Santi <smarnav2904@gmail.com>
2023-08-11 12:34:32 +0200

    added loops for coords

--- test.cpp
+++ test.cpp
@@ -16,6 +16,45 @@ int main(){
     IfcSchema::IfcWall::list::ptr walls = file.instances_by_type<IfcSchema::IfcWall>();
 
     IfcSchema::IfcDoor::list::ptr doors = file.instances_by_type<IfcSchema::IfcDoor>();
+
     
+    for (IfcSchema::IfcWindow::list::it it = windows->begin(); it != windows->end(); ++it) {
+		
+        IfcSchema::IfcObjectPlacement position;
+		const IfcSchema::IfcWindow* w = *it;
+		const IfcSchema::IfcWindow* window;
+
+		if ((window = w->as<IfcSchema::IfcWindow>()) != 0) {
+            position = window->ObjectPlacement();
+			
+		}
+
+	}
+
+    for (IfcSchema::IfcWall::list::it it = walls->begin(); it != walls->end(); ++it) {
+		
+        IfcSchema::IfcObjectPlacement position;
+		const IfcSchema::IfcWall* w = *it;
+		const IfcSchema::IfcWall* wall;
+        
+		if ((wall = w->as<IfcSchema::IfcWall>()) != 0) {
+            position = wall->ObjectPlacement();
+			
+		}
+
+	}
+
+    for (IfcSchema::IfcDoor::list::it it = doors->begin(); it != doors->end(); ++it) {
+		
+        IfcSchema::IfcObjectPlacement position;
+		const IfcSchema::IfcDoor* w = *it;
+		const IfcSchema::IfcDoor* door;
+        
+		if ((door = w->as<IfcSchema::IfcDoor>()) != 0) {
+            position = door->ObjectPlacement();
+			
+		}
+
+	}
 }
     

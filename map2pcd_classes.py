45e5e4939bf9e97ed54cd38c3853bde0d75d8a5b
Santi <smarnav2904@gmail.com>
2023-08-17 10:40:40 +0200

    Fill space between verts

--- map2pcd_classes.py
+++ map2pcd_classes.py
@@ -16,69 +16,111 @@ doors = model.by_type('IfcDoor')
 
 columns = model.by_type('IfcColumn')
 
-# Listas para almacenar las coordenadas
-all_coordinates = []
+zero_array = np.zeros(((220) + 1, (220) + 1, (4) + 1))
+
+
+
 
 for wall in walls:
     matrix = ifcopenshell.util.placement.get_local_placement(wall.ObjectPlacement)
-    all_coordinates.append((matrix[:, 3][:3], 1)) # 1 Representa pared
-
+    #zero_array.append((matrix[:, 3][:3], 1)) # 1 Representa pared
+    print("Funciona pared")
     shape = ifcopenshell.geom.create_shape(settings, wall)
     points = np.array(shape.geometry.verts).reshape(-1, 3)
     indices_a_eliminar = [3 - 1, 8 - 1]
     filtered_points = np.delete(points, indices_a_eliminar, axis=0)
 
-    for vert in filtered_points:
-        all_coordinates.append((vert, 1))
+    points = np.round(filtered_points).astype(int)
+
+    # Obtener coordenadas X y Z de los puntos
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2]
+    
+    # Calcular los valores máximos y mínimos
+    max_x = np.max(x_values)
+    min_x = np.min(x_values)
+    max_y = np.max(y_values)
+    min_y = np.min(y_values)
+    max_z = np.max(z_values)
+    min_z = np.min(z_values)
+
+    for x in range(min_x, max_x + 1):
+        for y in range(min_y, max_y + 1):
+            for z in range(min_z, max_z + 1):
+                if 0 <= x < 221 and 0 <= y < 221 and 0 <= z < 5:
+                    print("X:" + str(x) + " Y:" + str(y) + " Z:" + str(z))
+                    if(zero_array[x, y, z] == 0):
+                        zero_array[x, y, z] = 1
+
+    
 
 for door in doors:
     matrix = ifcopenshell.util.placement.get_local_placement(door.ObjectPlacement)
-    all_coordinates.append((matrix[:, 3][:3], 2)) # 2 Representa puerta
-
+    #zero_array.append((matrix[:, 3][:3], 2)) # 2 Representa puerta
+    print("Funciona puerta")
     shape = ifcopenshell.geom.create_shape(settings, door)
     points = np.array(shape.geometry.verts).reshape(-1, 3)
 
-    for vert in points:
-        all_coordinates.append((vert, 2))
+    points = np.round(points).astype(int)
+
+    # Obtener coordenadas X y Z de los puntos
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2]
+    
+    # Calcular los valores máximos y mínimos
+    max_x = np.max(x_values)
+    min_x = np.min(x_values)
+    max_y = np.max(y_values)
+    min_y = np.min(y_values)
+    max_z = np.max(z_values)
+    min_z = np.min(z_values)
+
+    for x in range(min_x, max_x + 1):
+        for y in range(min_y, max_y+1):
+            for z in range(min_z, max_z + 1):
+                if 0 <= x < 221 and 0 <= y < 221 and 0 <= z < 5:
+                    print("X:" + str(x) + " Y:" + str(y) + " Z:" + str(z))
+                    if(zero_array[x, y, z] == 0 or 1):
+                        zero_array[x, y, z] = 2
+
+    
     
 
 for column in columns:
     matrix = ifcopenshell.util.placement.get_local_placement(column.ObjectPlacement)
-    all_coordinates.append((matrix[:, 3][:3], 3)) # 3 Representa columna
-
+    #zero_array.append((matrix[:, 3][:3], 3)) # 3 Representa columna
+    print("Funciona columna")
     shape = ifcopenshell.geom.create_shape(settings, column)
     points = np.array(shape.geometry.verts).reshape(-1, 3)
 
-    for vert in points:
-        all_coordinates.append((vert, 3))
+    points = np.round(points).astype(int)
+
+    # Obtener coordenadas X y Z de los puntos
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2]
     
+    # Calcular los valores máximos y mínimos
+    max_x = np.max(x_values)
+    min_x = np.min(x_values)
+    max_y = np.max(y_values)
+    min_y = np.min(y_values)
+    max_z = np.max(z_values)
+    min_z = np.min(z_values)
 
-# Convertir la lista de coordenadas en una única matriz numpy
-#all_coordinates = np.array(all_coordinates)
+    for x in range(min_x, max_x + 1):
+        for y in range(min_y, max_y+1):
+            for z in range(min_z, max_z + 1):
+                if 0 <= x < 221 and 0 <= y < 221 and 0 <= z < 5:
+                    print("X:" + str(x) + " Y:" + str(y) + " Z:" + str(z))
+                    if(zero_array[x, y, z] == 0):
+                        zero_array[x, y, z] = 3
 
-max_x = np.max([coord[0] for coord, _ in all_coordinates])
-max_y = np.max([coord[1] for coord, _ in all_coordinates])
-max_z = np.max([coord[2] for coord, _ in all_coordinates])
-
-
-max_x = int(np.round(max_x))
-max_y = int(np.round(max_y))
-max_z = int(np.round(max_z))
-
-#print("Coordenadas máximas:")
-#print("X más alta:", max_x)
-#print("Y más alta:", max_y)
-#print("Z más alta:", max_z)
-
-
-resolution = 1
-
-zero_array = np.zeros(((max_x * resolution) + 1, (max_y * resolution) + 1, (max_z * resolution) + 1))
-
-for coord, elem_type in all_coordinates:
-    rounded_coord = tuple(round(value) for value in coord)
-    x, y, z = rounded_coord * resolution
-    zero_array[x, y, z] = elem_type
 
 print(zero_array)
+
+    
+
 
